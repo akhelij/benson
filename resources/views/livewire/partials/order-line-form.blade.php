e1ad1b993bfa66a5d76f4149ce91ef57ee556bf4
Akhelij <m.akhelij@gmail.com>
2025-10-14 12:27:51 +0100

    save the id of article not its name

--- resources/views/livewire/partials/order-line-form.blade.php
+++ resources/views/livewire/partials/order-line-form.blade.php
@@ -36,7 +36,7 @@
                         class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 @error('selectedArticle') border-red-500 @enderror">
                     <option value="">Sélectionner un article</option>
                     @foreach($articles as $article)
-                        <option value="{{ $article->nom }}">{{ $article->nom }}</option>
+                        <option value="{{ $article->id }}">{{ $article->nom }}</option>
                     @endforeach
                 </select>
                 @error('selectedArticle') <span class="text-red-500 text-xs mt-1">{{ $message }}</span> @enderror
